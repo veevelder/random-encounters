52c223bbb7ee806ff6170703e387197b64f56f38
veevelder <veevelder@gmail.com>
2022-03-06 16:40:04 -0700

    fixed logic when searching for encounters and with about-time

--- random-encounters.js
+++ random-encounters.js
@@ -1,4 +1,4 @@
-export class RandomEncounter {
+export default class RandomEncounter {
 	static printMessage(title, message){
 		let chatData = {
 			user : game.user.id,
@@ -23,170 +23,174 @@ export class RandomEncounter {
 		RandomEncounter.printMessage(title, message);
 	}
 	
-	static async checkRooms(scene, rooms) {
-		//if no room is defined then just return
-		if (rooms == "") {
-			return true;
+	static checkRooms(tokens, scene, rooms) {
+		console.debug("random-encounters | checking if tokens", tokens, "are in rooms", rooms)
+		//if no tokens/scene/room is defined then just return
+		if (rooms == "" || tokens.length == 0) {
+			return false;
 		}
-		let room_arr = []
-		if (rooms.includes(",")) {
-			room_arr = rooms.split(",");
-		}
-		else {
-			room_arr.push(rooms);
-		}
-		
-		//get a list of pc tokens
-		let pcs = game.actors.filter(a => a.hasPlayerOwner)
-		console.debug("random-encounters | checking if actors", pcs, "are in rooms", rooms)
+		let room_arr = rooms.split(",")
 		for (var r = 0; r < room_arr.length; r++) {
 			let room = scene.data.drawings.find(a => a.data.text == room_arr[r]);
 			if (room) {
 				room = room.data;
-				// loop over the pcs
-				for (var i = 0; i < pcs.length; i++) {
-					//get any active tokens
-					let tokens = await pcs[i].getActiveTokens()
-					for(var j = 0; j < tokens.length; j++) {
-						let point = {x: tokens[j].x, y: tokens[j].y};
-						//check for image rotation and fix token points to match
-						if (room.rotation) {
-							const r = (-room.rotation) * Math.PI / 180;
-							const center = {
-								x: room.x + room.width / 2, 
-								y: room.y + room.height / 2
-							}
-							point = {
-								x: center.x + (point.x - center.x) * Math.cos(r) - (point.y - center.y) * Math.sin(r),
-								y: center.y + (point.x - center.x) * Math.sin(r) + (point.y - center.y) * Math.cos(r)
-							}
+				// loop over tokens
+				for(var j = 0; j < tokens.length; j++) {
+					let point = {x: tokens[j].x, y: tokens[j].y};
+					//check for image rotation and fix token points to match
+					if (room.rotation) {
+						const r = (-room.rotation) * Math.PI / 180;
+						const center = {
+							x: room.x + room.width / 2, 
+							y: room.y + room.height / 2
 						}
-						//check if token is within the drawing box
-						const inBox = point.x >= room.x && point.x <= room.x + room.width && point.y >= room.y && point.y <= room.y + room.height;
-						if (inBox) {
-							//if its a rectangle then we good
-							if (room.type === CONST.DRAWING_TYPES.RECTANGLE) {
-								return true;
-							}
-							//make sure token is within the ellipse
-							if (room.type === CONST.DRAWING_TYPES.ELLIPSE) {
-								if (room.width && room.height) {
-									const dx = room.x + room.width / 2 - point.x;
-									const dy = room.y + room.height / 2 - point.y;
-									let in_ellipse = 4 * (dx * dx) / (room.width * room.width) + 4 * (dy * dy) / (room.height * room.height) <= 1;
-									if (in_ellipse) {
-										return true;
-									}
-								}
-							}
-							//check if token is within any of the points
-							if (room.type === CONST.DRAWING_TYPES.POLYGON) {
-								const cx = point.x - room.x;
-								const cy = point.y - room.y;
-								let w = 0;
-								for (let i0 = 0; i0 < room.points.length; ++i0) {
-									let i1 = i0 + 1 === room.points.length ? 0 : i0 + 1;
-									if (room.points[i0][1] <= cy && room.points[i1][1] > cy && (room.points[i1][0] - room.points[i0][0]) * (cy - room.points[i0][1]) - (room.points[i1][1] - room.points[i0][1]) * (cx - room.points[i0][0]) > 0) {
-										++w;
-									}
-									if (room.points[i0][1] > cy && room.points[i1][1] <= cy && (room.points[i1][0] - room.points[i0][0]) * (cy - room.points[i0][1]) - (room.points[i1][1] - room.points[i0][1]) * (cx - room.points[i0][0]) < 0) {
-										--w;
-									}
-								}
-								if (w !== 0) {
+						point = {
+							x: center.x + (point.x - center.x) * Math.cos(r) - (point.y - center.y) * Math.sin(r),
+							y: center.y + (point.x - center.x) * Math.sin(r) + (point.y - center.y) * Math.cos(r)
+						}
+					}
+					//check if token is within the drawing box
+					const inBox = point.x >= room.x && point.x <= room.x + room.width && point.y >= room.y && point.y <= room.y + room.height;
+					if (inBox) {
+						//if its a rectangle then we good
+						if (room.type === CONST.DRAWING_TYPES.RECTANGLE) {
+							return true;
+						}
+						//make sure token is within the ellipse
+						if (room.type === CONST.DRAWING_TYPES.ELLIPSE) {
+							if (room.width && room.height) {
+								const dx = room.x + room.width / 2 - point.x;
+								const dy = room.y + room.height / 2 - point.y;
+								let in_ellipse = 4 * (dx * dx) / (room.width * room.width) + 4 * (dy * dy) / (room.height * room.height) <= 1;
+								if (in_ellipse) {
 									return true;
 								}
-							}	
+							}
 						}
+						//check if token is within any of the points
+						if (room.type === CONST.DRAWING_TYPES.POLYGON) {
+							const cx = point.x - room.x;
+							const cy = point.y - room.y;
+							let w = 0;
+							for (let i0 = 0; i0 < room.points.length; ++i0) {
+								let i1 = i0 + 1 === room.points.length ? 0 : i0 + 1;
+								if (room.points[i0][1] <= cy && room.points[i1][1] > cy && (room.points[i1][0] - room.points[i0][0]) * (cy - room.points[i0][1]) - (room.points[i1][1] - room.points[i0][1]) * (cx - room.points[i0][0]) > 0) {
+									++w;
+								}
+								if (room.points[i0][1] > cy && room.points[i1][1] <= cy && (room.points[i1][0] - room.points[i0][0]) * (cy - room.points[i0][1]) - (room.points[i1][1] - room.points[i0][1]) * (cx - room.points[i0][0]) < 0) {
+									--w;
+								}
+							}
+							if (w !== 0) {
+								return true;
+							}
+						}	
 					}
 				}
 			}
 		}
-		
 		//if gotten this far no room matched so return false;
 		return false;
 	}
 	
-	static doRandomEncounters() {
+	static async doRandomEncounters(encounter = null) {
 		//if in combat dont do random encounter if not the gm dont do random encounter
 		if (game.combat || !game.users.filter(a => a.id == game.userId)[0].isGM) {
 			return false;
 		}
-
-		let active_scene = game.scenes.find(a => a.active).name;
-		console.log(`random-encounters | checking if scene '${active_scene}' has encounters`)
-		let encounters = game.settings.get("random-encounters", "encounters").filter(a => a.scene ==  active_scene);
-		for(var i = 0; i < encounters.length; i++) {
-			RandomEncounter.doRandomEncounter(encounters[i])
-		}
-	}
-	
-	static async doRandomEncounter(encounter) {
-		//if in combat dont do random encounter if not the gm dont do random encounter
-		if (game.combat || !game.users.filter(a => a.id == game.userId)[0].isGM) {
-			return false;
-		}
-		console.log("random-encounters | checking for random encounter")
-
+		//get active scene
 		let scene = game.scenes.find(a => a.active);
-		let doRollTable = false;
-		if (encounter.scene == scene.name) {
-			//check for daynight options
-			let do_check = false;
-			//check for day
-			if (encounter.daynight == "Day" && scene.data.darkness == 0) {
-				do_check = true;
-			}
-			else if (encounter.daynight == "Night" && scene.data.darkness == 1) {
-				do_check = true;
-			}
-			else if (encounter.daynight == "None") {
-				do_check = true;
-			}
-			console.debug("random-encounters | day/night/none encounter", do_check)
-			if (do_check) {
-				let inroom = await RandomEncounter.checkRooms(scene, encounter.rooms);
-				console.debug("random-encounters | room encounter", inroom)
-				if (inroom) {
-					//do roll check if one is set
-					if(encounter.chance == "") {
-						doRollTable = true;
+		console.log(`random-encounters | checking if scene '${scene.name}' has encounters`)
+		//get possible encounters based on scene, day/night/none options
+		let dayNight = scene.data.darkness == 0 ? "Day" : "Night"
+		//get pc tokens
+		let pc_tokens = []
+		let pcs = game.actors.filter(a => a.hasPlayerOwner)
+		for (var i = 0; i < pcs.length; i++) {
+			pc_tokens.push(await pcs[i].getActiveTokens()[0])
+		}
+		let encounters = []
+		if (encounter) {
+			console.debug("random-encounters | encounter was passed in checking conditions")
+			if (encounter.scene == scene.name && (encounter.daynight == dayNight || encounter.daynight == "None")) {
+				//if encounter is for a room
+				if (encounter.rooms != "") {
+					if (RandomEncounter.checkRooms(pc_tokens, scene, encounter.rooms)) {
+						console.debug("random-encounters | encounter for room and pcs are in room")
+						encounters.push(encounter)
 					}
-					else {
-						//do roll
-						var roll = new Roll(encounter.chance, {}).evaluate({"async":false}).total;
-						//check for range
-						if(encounter.onresult.includes("-")) {
-							var range = encounter.onresult.split("-");
-							//if roll is within range do table
-							if(roll >= parseInt(range[0]) && roll <= parseInt(range[1])) {
-								doRollTable = true;
-							}
-						}
-						//check roll against value
-						else {
-							if(roll == parseInt(encounter.onresult)) {
-								doRollTable = true;
-							}
-						}
+				}
+				else {
+					console.debug("random-encounters | checking for other room encounters and if pcs are in room")
+					//check if scene has rooms and if pcs are in the room
+					let rm_encounters = game.settings.get("random-encounters", "encounters")
+						.filter(a => a.scene ==  scene.name)
+						.filter(a => a.daynight == dayNight || a.daynight == "None")
+						.filter(a => a.rooms != "")
+						.filter(a => RandomEncounter.checkRooms(pc_tokens, scene, a.rooms))
+					if (rm_encounters.length == 0) {
+						encounters.push(encounter)
 					}
-					console.debug("random-encounters | die chance encounter", doRollTable)
 				}
 			}
 		}
-		
-		if(doRollTable) {
-			console.log(`random-encounters | random encounter '${encounter.name}' has been triggered`)
-			let tableRoll = await game.tables.find(t => t.name == encounter.rolltable).roll()
-			let tableResult = tableRoll.results[0]
-			let text = tableResult.data.text
-			if (tableResult.data.collection) {
-				text = "@Compendium[" + tableResult.data.collection + "." + tableResult.data.resultId + "]{" + text + "}"
-			}
-			RandomEncounter.printEncounter(encounter.name, text);
-		}
 		else {
-			RandomEncounter.printMessage(encounter.name, "No Random Encounter");
+			console.debug(`random-encounters | checking for room encounters`)
+			encounters = game.settings.get("random-encounters", "encounters")
+				.filter(a => a.scene ==  scene.name)
+				.filter(a => a.daynight == dayNight || a.daynight == "None")
+				.filter(a => a.rooms != "")
+				.filter(a => RandomEncounter.checkRooms(pc_tokens, scene, a.rooms))
+			
+			if (encounters.length == 0) {
+				console.debug(`random-encounters | no room encounters checking for scene encounters`)
+				encounters = game.settings.get("random-encounters", "encounters")
+					.filter(a => a.scene ==  scene.name)
+					.filter(a => a.daynight == dayNight || a.daynight == "None")
+					.filter(a => a.rooms == "")
+			}
+		}
+		//do encounter
+		for(var i = 0; i < encounters.length; i++) {
+			//Theres a random encounter run it
+			console.log("random-encounters | checking for random encounter")
+			let doRollTable = false;
+			//do roll check if one is set
+			if(encounters[i].chance == "") {
+				doRollTable = true;
+			}
+			else {
+				console.debug("random-encounters | roll for chance encounter")
+				//do roll
+				var roll = new Roll(encounters[i].chance, {}).evaluate({"async":false}).total;
+				//check for range
+				if(encounters[i].onresult.includes("-")) {
+					var range = encounters[i].onresult.split("-");
+					//if roll is within range do table
+					if(roll >= parseInt(range[0]) && roll <= parseInt(range[1])) {
+						doRollTable = true;
+					}
+				}
+				//check roll against value
+				else {
+					if(roll == parseInt(encounters[i].onresult)) {
+						doRollTable = true;
+					}
+				}
+			}
+			if(doRollTable) {
+				console.log(`random-encounters | random encounter '${encounters[i].name}' has been triggered`)
+				let tableRoll = await game.tables.find(t => t.name == encounters[i].rolltable).roll()
+				let tableResult = tableRoll.results[0]
+				let text = tableResult.data.text
+				if (tableResult.data.collection) {
+					text = "@Compendium[" + tableResult.data.collection + "." + tableResult.data.resultId + "]{" + text + "}"
+				}
+				RandomEncounter.printEncounter(encounters[i].name, text);
+			}
+			else {
+				RandomEncounter.printMessage(encounters[i].name, "No Random Encounter");
+			}
 		}
 	}
 }
@@ -290,7 +294,7 @@ export class RandomEncounterSettings extends FormApplication {
 					console.log("random-encounters | register encounter with about-time")
 					let doEncounter = async (encounter) => {
 						console.log("random-encounters | running from about-time")
-						RandomEncounter.doRandomEncounter(encounter);
+						RandomEncounter.doRandomEncounters(encounter);
 					}
 					encounters[i].timeout_id = game.Gametime.doEvery({minutes: encounters[i].time}, doEncounter, encounters[i])
 				}
@@ -335,7 +339,6 @@ export class RandomEncounterSettings extends FormApplication {
 		if (!el) {
 			return true;
 		}
-		
 		let encounters = game.settings.get("random-encounters", "encounters");
 		let rmEncounter = encounters[el.data("idx")]
 		game.Gametime.clearTimeout(rmEncounter.timeout_id)
@@ -386,7 +389,7 @@ Hooks.on("init", () => {
 			console.debug("random-encounters | hotkey pressed")
 			RandomEncounter.doRandomEncounters();
 		},
-		restricted: true, //gmonly
+		restricted: true,
 		precedence: CONST.KEYBINDING_PRECEDENCE.NORMAL
 	});
 });
@@ -394,7 +397,6 @@ Hooks.on("init", () => {
 Hooks.on("ready", function () {
 	if (!game.users.filter(a => a.id == game.userId)[0].isGM)
 		return true;
-	
 	//check for old settings and migrate
 	//some issues with the new version in that it was not migrating old settings when I fixed the name
 	console.debug("random-encounters | register settings")
@@ -405,7 +407,6 @@ Hooks.on("ready", function () {
 		type: RandomEncounterSettings,
 		restricted: true
 	});
-	
 	game.settings.register("random-encounters", "encounters", {
 		name: "",
 		hint: "",
@@ -414,7 +415,6 @@ Hooks.on("ready", function () {
 		default: [],
 		type: Object
 	});
-	
 	console.debug("random-encounters | checking for old settings")
 	var old_settings = []
 	game.settings.settings.forEach(a => {
@@ -422,14 +422,12 @@ Hooks.on("ready", function () {
 			old_settings.push(a)
 		}
 	})
-
 	if (old_settings.length != 0) {
 		Dialog.confirm({
 			title: "Random Encounters Settings Migration",
 			content: "<p>Found old Random Encounter settings do you want to keep the old settings?</p><p><strong>Yes</strong> will migrate and delete the old settings</p><p><strong>No</strong> will delete the old settings.</p>",
 			yes: () => {
 				for(var i = 0; i < old_settings.length; i++) {
-					console.log(old_settings[i])
 					game.settings.set("random-encounters", old_settings[i].key, old_settings[i].default);
 				}
 				game.settings.settings.forEach((v, k, m) => {
@@ -447,9 +445,7 @@ Hooks.on("ready", function () {
 					}
 				})
 			},
-			defaultYes: true
-			
+			defaultYes: true	
 		})
 	}
-	
 });
